7182b9a8e7595997066c259a6ab7d68c54a281b7
ferminarpone <arponefermin@gmail.com>
2024-06-10 15:26:27 -0300

    Listar personas

---
+++ Desafio2/src/main/java/listar_personas/Persona.java
@@ -0,0 +1,23 @@
+package listar_personas;
+
+
+public class Persona {
+    String nombre;
+    String apellido;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
