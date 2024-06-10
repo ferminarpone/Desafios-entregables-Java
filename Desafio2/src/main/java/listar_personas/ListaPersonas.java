69b0695b3ca4325f46bf44f812f4c5327843eec2
ferminarpone <arponefermin@gmail.com>
2024-06-10 16:40:01 -0300

    Listar personas

--- Desafio2/src/main/java/listar_personas/ListaPersonas.java
+++ Desafio2/src/main/java/listar_personas/ListaPersonas.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class ListaPersonas {
     private List<Persona> listaPersonas;
 
-    public ListaPersonas (){
+    public ListaPersonas() {
         this.listaPersonas = new ArrayList<>();
     }
 
@@ -19,17 +19,18 @@ public class ListaPersonas {
 
     public void agregarPersona(Persona persona) {
         this.listaPersonas.add(persona);
-        System.out.println("La persona con nombre "+ persona.nombre + " fue agregada exitosamente.");
+        System.out.println("La persona con nombre " + persona.nombre + " fue agregada exitosamente.");
     }
 
-// Mostrar lista sin sort
-    public  void mostrarLista(){
+    // Mostrar lista sin sort
+    public void mostrarLista() {
         System.out.println("Lista sin ordenar: ");
-        for (Persona persona : listaPersonas){
-            System.out.println(persona.getNombre() +" "+ persona.getApellido());
+        for (Persona persona : listaPersonas) {
+            System.out.println(persona.getNombre() + " " + persona.getApellido());
         }
         System.out.println(" ");
     }
+
     //Ordenar lista por nombre
     public void sortByName() {
         Collections.sort(listaPersonas, new Comparator<Persona>() {
@@ -39,8 +40,8 @@ public class ListaPersonas {
             }
         });
         System.out.println("Lista ordenada alfabeticamente por nombre: ");
-        for (Persona persona : listaPersonas){
-            System.out.println(persona.getNombre() +" "+ persona.getApellido());
+        for (Persona persona : listaPersonas) {
+            System.out.println(persona.getNombre() + " " + persona.getApellido());
         }
         System.out.println(" ");
     }
@@ -54,8 +55,8 @@ public class ListaPersonas {
             }
         });
         System.out.println("Lista ordenada alfabeticamente por apellido: ");
-        for (Persona persona : listaPersonas){
-            System.out.println(persona.getApellido() +" "+ persona.getNombre());
+        for (Persona persona : listaPersonas) {
+            System.out.println(persona.getApellido() + " " + persona.getNombre());
         }
 
         System.out.println(" ");
@@ -70,8 +71,8 @@ public class ListaPersonas {
             }
         });
         System.out.println("Lista ordenada alfabeticamente por apellido: ");
-        for (Persona persona : listaPersonas){
-            System.out.println(persona.getApellido() +" "+ persona.getNombre());
+        for (Persona persona : listaPersonas) {
+            System.out.println(persona.getApellido() + " " + persona.getNombre());
         }
     }
 }
