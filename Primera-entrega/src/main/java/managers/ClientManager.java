ad33f32d21f80d706d60272d23850fa2d3cef423
ferminarpone <arponefermin@gmail.com>
2024-06-19 14:10:22 -0300

    ClientManager

--- Primera-entrega/src/main/java/managers/ClientManager.java
+++ Primera-entrega/src/main/java/managers/ClientManager.java
@@ -6,36 +6,76 @@ import jakarta.persistence.*;
 import java.util.List;
 
 public class ClientManager {
-    public void create(String name, String lastName, Integer dni){
-        EntityManager manager = GenericManager.getEntityManager();
-        manager.getTransaction().begin();
-        Client cliente = new Client(name, lastName, dni);
-        manager.persist(cliente);
-        manager.getTransaction().commit();
-        manager.close();
+    public void create(String name, String lastName, Integer docNumber){
+        EntityManager manager = null;
+        EntityTransaction transaction;
+        try {
+            manager = GenericManager.getEntityManager();
+            transaction= manager.getTransaction();
+            transaction.begin();
+            Client cliente = new Client(name, lastName, docNumber);
+            manager.persist(cliente);
+            manager.getTransaction().commit();
+            System.out.println("Cliente creado exitosamente");
+        }catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            GenericManager.closeEntityManagerFactory();
+        }
     }
     public List<Client> readAll(){
-        EntityManager manager = GenericManager.getEntityManager();
-        List<Client> lista = manager.createQuery("From Client", Client.class).getResultList();
-        manager.close();
+        EntityManager manager = null;
+        List<Client> lista = null;
+        try {
+            manager = GenericManager.getEntityManager();
+            lista = manager.createQuery("From Client", Client.class).getResultList();
+
+        }catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (manager != null) {
+                manager.close();
+            }
+        }
         return lista;
     }
 
     public Client readById(Integer id){
-        EntityManager manager =  GenericManager.getEntityManager();
-        Client cliente = manager.find(Client.class, id);
-        manager.close();
-        return cliente;
+        EntityManager manager = null;
+        Client client = null;
+        try {
+            manager = GenericManager.getEntityManager();
+            client = manager.find(Client.class, id);
+        }catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (manager != null) {
+                manager.close();
+            }
+        }
+        return client;
     }
 
-    public void deleteById(Integer id){
-        EntityManager manager = GenericManager.getEntityManager();
-        manager.getTransaction().begin();
-        Client cliente = manager.find(Client.class, id);
-        if( cliente != null){
-            manager.remove(cliente);
-            manager.getTransaction().commit();
+    public void deleteById(Integer id) {
+
+        EntityManager manager = null;
+        EntityTransaction transaction;
+        try {
+            manager = GenericManager.getEntityManager();
+            transaction = manager.getTransaction();
+            transaction.begin();
+            Client client = manager.find(Client.class, id);
+            if (client != null) {
+                manager.remove(client);
+                manager.getTransaction().commit();
+            }
+            System.out.println("Cliente eliminado exitosamente");
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (manager != null) {
+                manager.close();
+            }
         }
-        manager.close();
     }
 }
