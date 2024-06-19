9f535b5ffc3b36390b86eed8a32fafac0d20f6e0
ferminarpone <arponefermin@gmail.com>
2024-06-19 14:27:50 -0300

    Product Manager

--- Primera-entrega/src/main/java/managers/ProductManager.java
+++ Primera-entrega/src/main/java/managers/ProductManager.java
@@ -1,43 +1,81 @@
 package managers;
 
+import entities.Client;
 import entities.Product;
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
 
 import java.util.List;
 
 public class ProductManager {
     public void create(String description, String code, Double price, Integer stock){
-        EntityManager manager = GenericManager.getEntityManager();
-        manager.getTransaction().begin();
-        Product producto = new Product(description, code, price, stock);
-        manager.persist(producto);
-        manager.getTransaction().commit();
-        manager.close();
+        EntityManager manager = null;
+        EntityTransaction transaction;
+        try {
+            manager = GenericManager.getEntityManager();
+            transaction= manager.getTransaction();
+            transaction.begin();
+            Product product = new Product(description, code, price, stock);
+            manager.persist(product);
+            manager.getTransaction().commit();
+            System.out.println("Producto creado exitosamente");
+        }catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            GenericManager.closeEntityManagerFactory();
+        }
     }
     public List<Product> readAll(){
-        EntityManager manager = GenericManager.getEntityManager();
-        List<Product> lista = manager.createQuery("From Product", Product.class).getResultList();
-        manager.close();
+        EntityManager manager = null;
+        List<Product> lista = null;
+        try {
+            manager = GenericManager.getEntityManager();
+            lista = manager.createQuery("From Product", Product.class).getResultList();
+        }catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (manager != null) {
+                manager.close();
+            }
+        }
         return lista;
     }
 
     public Product readById(Integer id){
-        EntityManager manager =  GenericManager.getEntityManager();
-        Product prducto = manager.find(Product.class, id);
-        manager.close();
-        return prducto;
-    }
-
-/*    public void deleteById(Integer id){
-        EntityManager manager = GenericManager.getEntityManager();
-        manager.getTransaction().begin();
-        Client cliente = manager.find(Client.class, id);
-        if( cliente != null){
-            manager.remove(cliente);
-            manager.getTransaction().commit();
+        EntityManager manager = null;
+        Product product = null;
+        try {
+            manager = GenericManager.getEntityManager();
+            product = manager.find(Product.class, id);
+        }catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (manager != null) {
+                manager.close();
+            }
         }
-        manager.close();
+        return product;
     }
- */
 
+    public void deleteById(Integer id) {
+        EntityManager manager = null;
+        EntityTransaction transaction;
+        try {
+            manager = GenericManager.getEntityManager();
+            transaction = manager.getTransaction();
+            transaction.begin();
+            Product product = manager.find(Product.class, id);
+            if (product != null) {
+                manager.remove(product);
+                manager.getTransaction().commit();
+            }
+            System.out.println("Producto eliminado exitosamente");
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (manager != null) {
+                manager.close();
+            }
+        }
+    }
 }
