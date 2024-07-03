51c93d8bb9c5ce678351cafc83cb60e5a0ef0a7b
ferminarpone <arponefermin@gmail.com>
2024-07-03 15:39:52 -0300

    Entities

--- Segunda-entrega/src/main/java/com/coder/ecommerce/entities/Client.java
+++ Segunda-entrega/src/main/java/com/coder/ecommerce/entities/Client.java
@@ -3,16 +3,27 @@ package com.coder.ecommerce.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "clients")
 @NoArgsConstructor @EqualsAndHashCode @ToString @AllArgsConstructor
 public class Client {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter @Setter private Long id;
+
     @Getter @Setter @NonNull  private String name;
+
     @Getter @Setter @NonNull  private String lastName;
+
     @Getter @Setter @NonNull  private Integer docNumber;
 
+    @Getter @Setter @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Invoice> invoices = new ArrayList<>();
+
+    @Getter @Setter @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Invoice_details> invoiceDetails = new ArrayList<>();
 }
 
 
